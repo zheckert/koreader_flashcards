09645255d38a9f1a379e3a5d1134aa6f0c4096bf
Zach Heckert <zach@zachheckert.com>
2025-02-26 12:08:25 -0500

    Add flashcard association in User model. Add functionality to create array of words from uploaded CSV data

--- kf_api/app/controllers/uploads_controller.rb
+++ kf_api/app/controllers/uploads_controller.rb
@@ -1,11 +1,12 @@
 class UploadsController < ApplicationController
-  # before_action :authenticate_user! todo: uncomment this when we need auth
+  # before_action :authenticate_user! todo: uncomment this when we need auth. Currently part of user spoofing
+
+  require 'csv'
 
   def create
     # Spoof user! todo: remove this and do it right
     @current_user = User.find(1)
 
-    puts current_user.inspect
     # CHeck and make sure a file is provided
     if params[:file].blank?
       return render json: { error: "No file uploaded" }, status: :unprocessable_entity
@@ -13,6 +14,14 @@ class UploadsController < ApplicationController
 
     # Attach file to user via ActiveStorage
     current_user.csv_file.attach(params[:file])
+
+    csv_data = current_user.csv_file.download
+
+    # In case you forget again, headers:true treats the first row as headers instead of data todo: delete this note when you remember
+    CSV.parse(csv_data, headers: true)
+       .map { |row| row["word"] }
+       .compact_blank # IN CASE YOU FORGET! This removes nil or blank values. They shouldn't exist at this stage but better to be safe. todo: update or remove note
+       .each { |word| current_user.flashcards.create!(word: word) }
 
     render json: { message: "File uploaded successfully" }, status: :created
   end
--- kf_api/app/models/user.rb
+++ kf_api/app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Currently, each user gets one CSV at a time.
   has_one_attached :csv_file
+
+  # A user can have as many flashcards as there are words in their upload.
+  has_many :flashcards
 end
