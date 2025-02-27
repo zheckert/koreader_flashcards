a3cf1e620681b842758eb886aee53a7b23fed275
Zach Heckert <zach@zachheckert.com>
2025-02-27 11:36:37 -0500

    WIP definitions pieces

--- kf_api/app/controllers/uploads_controller.rb
+++ kf_api/app/controllers/uploads_controller.rb
@@ -15,14 +15,18 @@ class UploadsController < ApplicationController
     # Attach file to user via ActiveStorage
     current_user.csv_file.attach(params[:file])
 
-    csv_data = current_user.csv_file.download
+    csv_data = current_user.csv_file.download.force_encoding("UTF-8")
 
     # In case you forget again, headers:true treats the first row as headers instead of data todo: delete this note when you remember
-    CSV.parse(csv_data, headers: true)
-       .map { |row| row["word"] }
-       .compact_blank # IN CASE YOU FORGET! This removes nil or blank values. They shouldn't exist at this stage but better to be safe. todo: update or remove note
-       .each { |word| current_user.flashcards.create!(word: word) }
+    words = CSV.parse(csv_data, headers: true)
+                .map { |row| row["word"] }
+                .compact_blank # IN CASE YOU FORGET! This removes nil or blank values. They shouldn't exist at this stage but better to be safe. todo: update or remove note
 
-    render json: { message: "File uploaded successfully" }, status: :created
+    flashcards = words.map { |word| current_user.flashcards.create!(word: word) }
+
+    # Queue jobs to grab definitions for each word
+    flashcards.each { |flashcard| FetchDefinitionJob.perform_later(flashcard.id) }
+
+    render json: { message: "File uploaded successfully, and definitions are being fetched" }, status: :created
   end
 end
--- kf_api/db/schema.rb
+++ kf_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_27_152500) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_27_162927) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_27_152500) do
   create_table "definitions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "flashcard_id"
   end
 
   create_table "flashcards", force: :cascade do |t|
