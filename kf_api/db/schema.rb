af72755d461e11124365487ffa820909356597ce
Zach Heckert <zach@zachheckert.com>
2025-03-06 10:50:59 -0500

    Add unique index to prevent duplications. Use find_or_create to reuse existing flashcards

--- kf_api/db/schema.rb
+++ kf_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_165615) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_154142) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_165615) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "word"], name: "index_flashcards_on_user_id_and_word", unique: true
     t.index ["user_id"], name: "index_flashcards_on_user_id"
   end
 
