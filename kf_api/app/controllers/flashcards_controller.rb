4afde4a30f7abbcf5362ee653e2696f0a2c340a8
Zach Heckert <zach@zachheckert.com>
2025-03-19 11:58:23 -0400

    Flesh out index method in flashcards_controller. Implement generic cors solutions. Install axios and implement basic frontend functionality to get words on the page

--- kf_api/app/controllers/flashcards_controller.rb
+++ kf_api/app/controllers/flashcards_controller.rb
@@ -1,5 +1,7 @@
 class FlashcardsController < ApplicationController
   def index
+    @flashcards = User.find(1).flashcards.includes(:definition)
+    render json: @flashcards, include: :definition
   end
 
   def destroy
--- kf_frontend/src/App.jsx
+++ kf_frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3000/flashcards")
+      .then((response) => setFlashcards(response.data))
+      .catch((error) => console.error("Error fetching flashcards:", error));
+  });
+
   return (
     <>
-      <div>Hello, world!</div>
+      {flashcards.map((card) => (
+        <div key={card.id} style={{ display: "flex", gap: "10px" }}>
+          <div>{card.word}:</div>
+          <div>{card.definition?.text}</div>
+        </div>
+      ))}
     </>
   );
 }
