69fadb6e7176cfcd740d402a4317e3594fb82373
Zach Heckert <zach@zachheckert.com>
2025-03-19 12:01:09 -0400

    Add missing dependency array in useEffect

--- kf_frontend/src/App.jsx
+++ kf_frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       .get("http://127.0.0.1:3000/flashcards")
       .then((response) => setFlashcards(response.data))
       .catch((error) => console.error("Error fetching flashcards:", error));
-  });
+  }, []);
 
   return (
     <>
